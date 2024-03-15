2b96a3c33964966c6e191d48fecd33fcbab4160f
LeenTD <trandanglen2002@gmail.com>
2024-03-15 14:12:11 +0700

    camera follow done

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,22 +1,51 @@
-using UnityEngine;
+﻿using UnityEngine;
+using Photon.Pun;
+using System.Collections.Generic;
 
 public class CameraFollow : MonoBehaviour
 {
     public float followSpeed = 2f;
-    public Transform target;
+    private Transform target;
 
-    // Update is called once per frame
+    void Start()
+    {
+        
+            // Tìm tất cả các đối tượng trong danh sách GameObject với tag "Player"
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+            // Tìm player của mình trong danh sách và gán làm target
+            foreach (GameObject player in players)
+            {
+                PhotonView playerPhotonView = player.GetComponent<PhotonView>();
+                if (playerPhotonView != null && playerPhotonView.IsMine)
+                {
+                    target = player.transform;
+                    break; // Kết thúc vòng lặp sau khi tìm thấy target
+                }
+            }
+
+            // Nếu không tìm thấy player của mình trong danh sách, thông báo lỗi
+            if (target == null)
+            {
+                Debug.LogError("Player object not found with tag 'Player'.");
+            }
+        
+    }
+
     void Update()
     {
         if (target != null)
         {
+            // Cập nhật vị trí camera
             Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
             transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
         }
+ 
+    }
+    
+    public void setPlayer(GameObject player)
+    {
+        target = player.transform;
     }
 
-    public void SetTarget(Transform newTarget)
-    {
-        target = newTarget;
-    }
 }
--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -1,3 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public List<Vector2> spawnPositions;
+    public LayerMask playerLayer;
+
+    private void Start()
+    {
+        var playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+
+        if(playerCount <= 4)
+        {
+
+            GameObject.FindAnyObjectByType<CameraFollow>().setPlayer(PhotonNetwork.Instantiate(playerPrefab.name, spawnPositions[playerCount - 1], Quaternion.identity));
+        }
+    }
+}
+
+/*
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
@@ -15,7 +39,9 @@ public class SpawnPlayer : MonoBehaviour
 
         if(playerCount <= 4)
         {
-            PhotonNetwork.Instantiate(playerPrefab.name, spawnPositions[playerCount - 1], Quaternion.identity);
+            GameObject playerObject = PhotonNetwork.Instantiate(playerPrefab.name, spawnPositions[playerCount - 1], Quaternion.identity);
+            playerObject.tag = "Player"; // Gắn tag "Player" cho đối tượng player.
         }
     }
 }
+*/
