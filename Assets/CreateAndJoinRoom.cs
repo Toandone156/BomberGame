e0ff58968188e07a454a709111378fe0283f62e0
Nhon Tran <nhonvn2509@gmail.com>
2024-03-17 14:11:31 +0700

    handle exception exist room, empty field, popup

--- Assets/CreateAndJoinRoom.cs
+++ Assets/CreateAndJoinRoom.cs
@@ -1,4 +1,6 @@
+﻿using EasyUI.Popup;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 using WebSocketSharp;
@@ -8,50 +10,104 @@ public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
     public TMP_InputField roomName;
     public TMP_InputField nicknameInput;
     [SerializeField] private AudioSource clickSoundEffect;
-
     private void Start()
     {
-        if(nicknameInput != null)
+        if (nicknameInput != null)
         {
             var nickname = PhotonNetwork.NickName;
             var savename = PlayerPrefs.GetString("Nickname");
-
             nicknameInput.text = !string.IsNullOrEmpty(nickname) ? nickname : (savename.IsNullOrEmpty() ? savename : "");
         }
     }
+    public override void OnCreatedRoom()
+    {
 
+        Debug.Log(PhotonNetwork.CountOfPlayersInRooms);
+        Debug.Log("Ok create");
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Popup.Show("Error", "Exist room", "Close", PopupColor.Magenta);
+        Debug.Log("Exist room");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        if (returnCode == ErrorCode.GameDoesNotExist)
+        {
+            // Hiển thị thông báo cho người chơi
+            Popup.Show("Exist room", "Do not exist room. Please change your room name.", "OK", PopupColor.Magenta);
+            Debug.Log("Phòng không tồn tại. Vui lòng kiểm tra lại tên phòng.");
+        }
+        else
+        {
+            // Xử lý các trường hợp lỗi khác tùy theo nhu cầu của bạn
+            Debug.Log("Lỗi khi tham gia phòng: " + message);
+        }
+    }
     public void CreateRoom()
     {
         clickSoundEffect.Play();
 
-
-
-        PhotonNetwork.CreateRoom(roomName.text);
-
-        if (PhotonNetwork.InLobby)
+        if (string.IsNullOrWhiteSpace(roomName.text))
         {
+            Popup.Show("Empty room name", "Enter room name field", "OK", PopupColor.Magenta);
+            Debug.Log("Vui lòng nhập tên phòng.");
+            return;
+        }
+        if (string.IsNullOrWhiteSpace(nicknameInput.text))
+        {
+            Popup.Show("Empty nick name", "Enter nick name field", "OK", PopupColor.Magenta);
+            Debug.Log("Vui lòng nhập nick name");
+            return;
 
         }
+        if (string.IsNullOrWhiteSpace(nicknameInput.text) && string.IsNullOrWhiteSpace(roomName.text))
+        {
+            Popup.Show("Empty field", "Enter all field", "OK", PopupColor.Magenta);
+            return;
+        }
+        // Tạo RoomOptions với số lượng người chơi tối đa là 4
+        RoomOptions roomOptions = new RoomOptions { MaxPlayers = 4 };
 
-    }
-
-    public void JoinRoom()
-    {
-        clickSoundEffect.Play();
-        PhotonNetwork.JoinRoom(roomName.text);
+        PhotonNetwork.CreateRoom(roomName.text, roomOptions);
+        PhotonNetwork.CreateRoom(roomName.text);
     }
 
     public override void OnJoinedRoom()
     {
         //PhotonNetwork.LoadLevel("MainScene");
+
         clickSoundEffect.Play();
         PhotonNetwork.NickName = nicknameInput.text;
         PlayerPrefs.SetString("Nickname", PhotonNetwork.NickName);
         PhotonNetwork.LoadLevel("waitingRoom");
     }
+    public void JoinRoom()
+    {
+        if (string.IsNullOrWhiteSpace(nicknameInput.text) && string.IsNullOrWhiteSpace(roomName.text))
+        {
+            Popup.Show("Empty field", "Enter all field", "OK", PopupColor.Magenta);
+            return;
+        }
+        if (string.IsNullOrWhiteSpace(roomName.text))
+        {
+            Popup.Show("Empty room name", "Enter room name field", "OK", PopupColor.Magenta);
+            Debug.Log("Vui lòng nhập tên phòng.");
+            return;
+        }
+        if (string.IsNullOrWhiteSpace(nicknameInput.text))
+        {
+            Popup.Show("Empty nick name", "Enter nick name field", "OK", PopupColor.Magenta);
+            Debug.Log("Vui lòng nhập nick name");
+            return;
 
+        }
+        clickSoundEffect.Play();
+        PhotonNetwork.JoinRoom(roomName.text);
+    }
     public override void OnLeftRoom()
     {
+
         PhotonNetwork.LoadLevel("Lobby");
     }
 
@@ -77,5 +133,17 @@ public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
     {
         clickSoundEffect.Play();
         Application.Quit();
+    }
+
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("Player entered the room: " + newPlayer.NickName);
+        if (PhotonNetwork.CurrentRoom.PlayerCount > PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            Popup.Show("Out of total", "Quá số lượng người chơi là 4", "OK", PopupColor.Magenta);
+            Debug.Log("Số lượng người chơi vượt quá quy định.");
+            return;
+        }
     }
 }
---
+++ Assets/RoomListing.cs
@@ -0,0 +1,13 @@
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomListing : MonoBehaviour
+{
+    [SerializeField]
+    private Text _text;
+    public void SetRoomInfo(RoomInfo roomInfo)
+    {
+        _text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
+    }
+}
